6596c6c574fe98757726c28ba919f1fbaa8db6ac
CrisAcalo <cristianchoacalo@gmail.com>
2024-02-01 12:13:18 -0500

    fixing errors

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-400 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-100 shadow-md overflow-hidden sm:rounded-lg">
                 <div class="h-32">
                     <a href="/">
                         <x-application-logo class="w-20 h-20 fill-current text-gray-100" />
